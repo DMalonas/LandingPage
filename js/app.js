7a6ff86ec4c91cf4456a24d2b629ae10e1f4c794
DimitriosMalonas <dimitriosmalonas@outlook.com>
2019-11-06 23:37:51 +0000

    Section active state and scroll to anchor functionalities added

--- js/app.js
+++ js/app.js
@@ -17,6 +17,44 @@
  * Define Global Variables
  * 
 */
+const sections = document.querySelectorAll('section');
+const divs = document.querySelectorAll('div');
+const container = document.getElementsByClassName('landing__container');
+
+
+for (let i = 0; i < sections.length; i++) {
+    let node = document.createElement('li');
+	let link = document.createElement('a');
+	link.setAttribute('href', sections[i].id);
+	link.innerHTML = sections[i].id;
+    node.appendChild(link);
+    document.getElementById("navbar__list").appendChild(node);
+}
+
+
+
+const links = document.querySelectorAll('a');
+const thirdLink = links[2];
+for (let i = 0; i < links.length; i++) { 
+    links[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log("Look, ma! We didn't navigate to a new page!");
+        //let elmnt = document.getElementById("section1");
+        sections[i].scrollIntoView();
+    });
+}
+
+
+for (let i = 0; i < sections.length; i++) {
+    sections[i].addEventListener("mouseenter", function() {
+      let current = document.getElementsByClassName("your-active-class");
+      console.log(current.className);
+      current[0].className = current[0].className.replace("your-active-class", "");
+      this.className += "your-active-class";
+      console.log("We are inside " + sections[i].id);
+    });
+  } 
+
 
 
 /**
@@ -24,16 +62,6 @@
  * Start Helper Functions
  * 
 */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
 
 
 // Add class 'active' to section when near top of viewport
